a5317d77062682efde925dff5dd69b674c52146b
Thomas Pienaar <thomas.pienaar@icemobile.com>
2017-04-06 14:57:50 +0200

    Third commit

--- app/src/main/java/com/dummy/test/presentationapp/MainActivity.java
+++ app/src/main/java/com/dummy/test/presentationapp/MainActivity.java
@@ -1,10 +1,7 @@
 package com.dummy.test.presentationapp;
 
-import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.AttributeSet;
-import android.view.View;
 
 import com.dummy.test.presentationapp.adapter.FlowAdapter;
 import com.dummy.test.presentationapp.view.FlowLayout;
@@ -15,15 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    public View onCreateView(View parent, String name, Context context, AttributeSet attrs) {
-
-        View baseView = super.onCreateView(parent, name, context, attrs);
-//        FlowLayout flowLayout = (FlowLayout) baseView.findViewById(R.id.flow_layout);
-//        flowLayout.setAdapter(new FlowAdapter());
-        return baseView;
-
+        FlowLayout flowLayout = (FlowLayout) findViewById(R.id.flow_layout);
+        flowLayout.setAdapter(new FlowAdapter());
     }
 }
+
+
